784676203f9db4571c398315a043d5619770d2bb
Ítalo Sousa <italusousa@gmail.com>
2018-01-21 14:57:09 -0300

    pesquisa zoada

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +15,25 @@ export class HeaderComponent implements OnInit {
   inscricao: Subscription;
   showItens: Boolean = false;
 
+  startups: Observable<any[]>;
+  itemsRef: AngularFireList<any>;
+  
   constructor(
     private route: ActivatedRoute,
-    private router: Router
-  ) { } 
+    private router: Router,
+    private db: AngularFireDatabase
+  ) { 
+    this.itemsRef = db.list('pitches', ref=> ref.orderByChild('titulo'));    
+    this.startups = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+   } 
+   
+
+   ir(value){
+     console.log(value);
+     
+    }
 
   ngOnInit() {
     this.inscricao = this.route.params.subscribe(
